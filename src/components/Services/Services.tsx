4c9babb40a4d13e21ef2e0b9ce58a79f6fb84b2e
najeesimmons <najee.bryant.simmons@gmail.com>
2024-07-25 15:45:29 -0400

    add mw div to main components

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -9,30 +9,32 @@ export default function Services() {
       className={classes.wrapper}
       style={{ backgroundColor: theme.colors.purpleJen[7] }}
     >
-      <div className={classes.body}>
-        <Title className={classes.title}>Services</Title>
-        <Text fw={500} fz="lg" mb={5}>
-          Subscribe to our newsletter!
-        </Text>
-        <Text fz="sm" c="dimmed">
-          You will never miss important product updates, latest news and
-          community QA sessions. Our newsletter is once a week, every Sunday.
-        </Text>
+      <div style={{ maxWidth: 900, display: "flex", margin: "0 auto" }}>
+        <div className={classes.body}>
+          <Title className={classes.title}>Services</Title>
+          <Text fw={500} fz="lg" mb={5}>
+            Subscribe to our newsletter!
+          </Text>
+          <Text fz="sm" c="dimmed">
+            You will never miss important product updates, latest news and
+            community QA sessions. Our newsletter is once a week, every Sunday.
+          </Text>
 
-        <div className={classes.controls}>
-          <TextInput
-            placeholder="Your email"
-            classNames={{ input: classes.input, root: classes.inputWrapper }}
-          />
-          <Button className={classes.control}>Subscribe</Button>
+          <div className={classes.controls}>
+            <TextInput
+              placeholder="Your email"
+              classNames={{ input: classes.input, root: classes.inputWrapper }}
+            />
+            <Button className={classes.control}>Subscribe</Button>
+          </div>
         </div>
+        <Image
+          src={"./undraw_open_note_gwm9.svg"}
+          className={classes.image}
+          // TODO: alt tag
+          alt=""
+        />
       </div>
-      <Image
-        src={"./undraw_open_note_gwm9.svg"}
-        className={classes.image}
-        // TODO: alt tag
-        alt=""
-      />
     </div>
   );
 }
